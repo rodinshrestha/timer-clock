35d23edc3a669192ccde36953955a9c4b506bf04
HUMAN <rodinshrestha@gmail.com>
2020-09-03 10:33:27 +0545

    functional buttons(pause, resume, reset)  added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Button from "./Components/Button";
 
 function App() {
   const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [inter, setInter] = useState();
+  const [status, setStatus] = useState(0);
   const start = () => {
     run();
-    setInterval(run, 10);
+    setStatus(1);
+    setInter(setInterval(run, 0.1));
   };
 
   let updatedMs = time.ms,
@@ -31,12 +34,32 @@ function App() {
     updatedMs++;
     return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
   };
+
+  const pause = () => {
+    clearInterval(inter);
+    setStatus(2);
+  };
+
+  const resume = () => start();
+
+  const reset = () => {
+    clearInterval(inter);
+    setStatus(0);
+    return setTime({ ms: 0, s: 0, m: 0, h: 0 });
+  };
+
   return (
     <div className="main-section">
       <div className=" clock-holer">
         <div className="stopwatch">
           <Display time={time} />
-          <Button start={start} />
+          <Button
+            start={start}
+            status={status}
+            reset={reset}
+            pause={pause}
+            resume={resume}
+          />
         </div>
       </div>
     </div>
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,11 +1,43 @@
 import React from "react";
 import "../App.css";
 
-const Button = ({ start }) => {
+const Button = ({ start, status, reset, pause, resume }) => {
   return (
-    <button className="stopwatch-btn stopwatch-btn-gre" onClick={start}>
-      Start
-    </button>
+    <div>
+      {status === 0 ? (
+        <button className="stopwatch-btn stopwatch-btn-gre" onClick={start}>
+          Start
+        </button>
+      ) : (
+        ""
+      )}
+
+      {status === 1 ? (
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-red" onClick={pause}>
+            Pause
+          </button>
+          <button className="stopwatch-btn stopwatch-btn-yel" onClick={reset}>
+            Reset
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      {status === 2 ? (
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-gre" onClick={resume}>
+            Resume
+          </button>
+          <button className="stopwatch-btn stopwatch-btn-yel" onClick={reset}>
+            Reset
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
   );
 };
 
